34ec180e2b6657428d1846bf5c9a1d6a058e9ae7
oObelix <oobelix@yandex.ru>
2021-07-26 15:04:28 +0300

    db_init.py - refactoring functions to class CollectedData methods

--- db_init.py
+++ db_init.py
@@ -1,9 +1,8 @@
-from typing import List, Tuple, Any, Iterator
+from typing import List, Tuple, Any, Iterator, ClassVar
 from config import Config
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from datetime import datetime
 from sqlalchemy.orm import declarative_base, sessionmaker
-
 
 config = Config()
 engine: Any = create_engine(config.db_server_dsn)
@@ -22,6 +21,44 @@ class CollectedData(Base):
     end_ip_address: str = Column(String(15))
     total_count: str = Column(String(15))
 
+    def get_collected_data(self, session: Any) -> List[ClassVar]:
+        """
+        Get all records in get_collected table and return as list of instances
+        of CollectedData
+        :return: List[CollectedData]
+        """
+        # session: Any = sessionmaker(bind=engine)()
+        return session.query(self).all()
+
+    def update_collected_data(self,
+                              session: Any,
+                              begin_ip_address: List[str],
+                              end_ip_address: List[str],
+                              total_count: List[str]) -> None:
+        """
+        Replace all data of update_collected with new data
+        :param begin_ip_address: List[str]
+        :param end_ip_address: List[str]
+        :param total_count: List[str]
+        :return: None
+        """
+        CollectedData.__table__.drop(engine)
+        Base.metadata.create_all(engine, tables=[CollectedData.__table__])
+
+        data: Iterator[Tuple[str, str, str]] = \
+            zip(begin_ip_address, end_ip_address, total_count)
+
+        # session: Any = sessionmaker(bind=engine)()
+        session.bulk_insert_mappings(
+            CollectedData,
+            [dict(
+                begin_ip_address=bia,
+                end_ip_address=eia,
+                total_count=tc
+            ) for bia, eia, tc in data]
+        )
+        session.commit()
+
 
 class Users(Base):
     """
@@ -35,44 +72,6 @@ class Users(Base):
     created_at: DateTime = Column(DateTime(), default=datetime.utcnow,
                                   nullable=False)
     last_request: DateTime = Column(DateTime())
-
-
-def get_collected_data() -> List[CollectedData]:
-    """
-    Get all records in get_collected table and return as list of instances
-    of CollectedData
-    :return: List[CollectedData]
-    """
-    session: Any = sessionmaker(bind=engine)()
-    return session.query(CollectedData).all()
-
-
-def update_collected_data(begin_ip_address: List[str],
-                          end_ip_address: List[str],
-                          total_count: List[str]) -> None:
-    """
-    Replace all data of update_collected with new data
-    :param begin_ip_address: List[str]
-    :param end_ip_address: List[str]
-    :param total_count: List[str]
-    :return: None
-    """
-    CollectedData.__table__.drop(engine)
-    Base.metadata.create_all(engine, tables=[CollectedData.__table__])
-
-    data: Iterator[Tuple[str, str, str]] =\
-        zip(begin_ip_address, end_ip_address, total_count)
-
-    session: Any = sessionmaker(bind=engine)()
-    session.bulk_insert_mappings(
-        CollectedData,
-        [dict(
-            begin_ip_address=bia,
-            end_ip_address=eia,
-            total_count=tc
-        ) for bia, eia, tc in data]
-    )
-    session.commit()
 
 
 if __name__ == "__main__":
