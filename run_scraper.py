bf085e38f3f39eb36e5096f8b931b45e822482e9
oObelix <oobelix@yandex.ru>
2021-07-27 11:17:02 +0300

    run_scraper.py - added requests headers and allow_redirect

--- run_scraper.py
+++ run_scraper.py
@@ -10,6 +10,11 @@ URL_PARSE = "https://lite.ip2location.com/russian-federation-ip-address-ranges"
 XPATH_BEGIN_IP_ADDRESS = ".//table[@id='ip-address']/tbody/tr/td[2]/text()"
 XPATH_END_IP_ADDRESS = ".//table[@id='ip-address']/tbody/tr/td[4]/text()"
 XPATH_TOTAL_COUNT = ".//table[@id='ip-address']/tbody/tr/td[6]/text()"
+REQUESTS_HEADERS = {'Cache-Control': "no-cache",
+                    'User-Agent': "Mozilla/5.0 (Macintosh; Intel "
+                                  "Mac OS X 10_15_7) AppleWebKit/605.1.15 "
+                                  "(KHTML, like Gecko) Version/14.1.2 "
+                                  "Safari/605.1.15"}
 
 
 def print_console(*lists: List[Tuple]) -> None:
@@ -23,7 +28,9 @@ def print_console(*lists: List[Tuple]) -> None:
 
 
 if __name__ == "__main__":
-    request: Any = requests.get(URL_PARSE)
+    request: Any = requests.get(URL_PARSE,
+                                headers=REQUESTS_HEADERS,
+                                allow_redirects=True)
     html_parse: str = request.text
 
     parse_tree: Any = html.fromstring(html_parse)
